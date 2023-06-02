c0cff99be8cc0f48f0542e8007689b5947a5e796
shivam086r <106476217+shivam086r@users.noreply.github.com>
2023-06-02 20:48:43 +0530

    Update InsertToDo.js

--- InsertToDo.js
+++ InsertToDo.js
@@ -2,30 +2,30 @@
 const { v4 } = require("uuid");
 const AWS = require("aws-sdk");
 
-const kaamBharo = async (event) => {
+const InsertToDo = async (event) => {
 
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  const { kaam } = JSON.parse(event.body);
+  const { work } = JSON.parse(event.body);
   const createdAt = new Date().toISOString();
   const id = v4();
-  const newKaam = {
+  const newWork = {
     id,
-    kaam,
+    work,
     createdAt,
     completed: false
   }
   await dynamoDb.put({
-    TableName: "KaamKaro",
-    Item: newKaam
+    TableName: "InsertToDo",
+    Item: newWork
   }).promise();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(newKaam),
+    body: JSON.stringify(newWork),
     };
 };
 
 module.exports = {
-    handler: kaamBharo,
+    handler: InsertToDo,
 };
